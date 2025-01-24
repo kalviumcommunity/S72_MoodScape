f52c914264804234baeaeab3d355fd45879d954e
Aayush-207 <aayushraut2006@gmail.com>
2025-01-24 14:54:48 +0530

    APItesting

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/greet', (req, res) => {
-  res.send('Welcome to the MoodScape!');
+app.get('/ping', (req, res) => {
+  res.send('pong');
 });
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
