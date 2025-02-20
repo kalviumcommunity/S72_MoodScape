06efcdd1f0e19d23446630c105962c49505ab934
Aayush-207 <aayushraut2006@gmail.com>
2025-02-20 16:33:13 +0530

    -

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+// client/src/App.js
+import React from 'react';
+import LandingPage from './Home';
+
+function App() {
+  return (
+    <div className="App">
+      <LandingPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Home.jsx
@@ -0,0 +1,14 @@
+// client/src/LandingPage.js
+import React from 'react';
+
+function LandingPage() {
+  return (
+    <div className="landing-page">
+      <h1>Welcome to ASAP</h1>
+      <p>Learn more about our awesome idea!</p>
+      <button>Get Started</button>
+    </div>
+  );
+}
+
+export default LandingPage;
