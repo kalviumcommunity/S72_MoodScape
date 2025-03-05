1c6626d61148834f573913a0dc4055d5c158165a
Aayush-207 <aayushraut2006@gmail.com>
2025-03-06 02:22:12 +0530

    -

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -4,8 +4,8 @@ import React from 'react';
 function LandingPage() {
   return (
     <div className="landing-page">
-      <h1>Welcome to ASAP</h1>
-      <p>Learn more about our awesome idea!</p>
+      <h1>Welcome to MOODSCAPES</h1>
+      <p>Lets check out whats your mood today☺️</p>
       <button>Get Started</button>
     </div>
   );
